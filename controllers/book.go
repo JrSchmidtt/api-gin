9f93b51c3582732266d10a581eda4d334e3d365b
jrschmidtt <junior.schmidt.99@gmail.com>
2022-10-04 06:57:26 -0300

    controllers

--- controllers/book.go
+++ controllers/book.go
@@ -1,9 +1,53 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"strconv"
 
-func GetBooks(c *gin.Context){
-	c.JSON(200, gin.H{
-		"value": "ok",
-	})
+	"github.com/JrSchmidtt/api-gin/database"
+	"github.com/JrSchmidtt/api-gin/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetBook(c *gin.Context){
+	id := c.Param("id")
+
+	newid, err := strconv.Atoi(id)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "ID has to be integer",
+		})
+		return
+	}
+	db := database.GetDatabase()
+	var book models.Book
+	err = db.First(&book, newid).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Not found" + err.Error(),
+		})
+		return
+	}
+	c.JSON(200, book)
+}
+
+func CreateBook(c *gin.Context){
+	db := database.GetDatabase()
+
+	var book models.Book
+
+	err := c.ShouldBindJSON(&book)
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot bind JSON: "+ err.Error(),
+		})
+		return
+	}
+
+	err = db.Create(&book).Error
+	if err != nil {
+		c.JSON(400, gin.H{
+			"error": "Cannot create book: "+ err.Error(),
+		})
+		return
+	}
 }
