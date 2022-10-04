241ff13f7b41e618244767a17a5c179f303cdcea
jrschmidtt <junior.schmidt.99@gmail.com>
2022-10-03 21:00:22 -0300

    database module

---
+++ database/database.go
@@ -0,0 +1,29 @@
+package database
+
+import (
+	"log"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func StartDB(){
+	str := "host=localhost port=25432 user=admin dbname=books sslmode=disable password=123456"
+	db, err := gorm.Open(postgres.Open(str), &gorm.Config{})
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+	config, _ := db.DB()
+
+	config.SetConnMaxIdleTime(10)
+	config.SetMaxIdleConns(10)
+	config.SetMaxOpenConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
